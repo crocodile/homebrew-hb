e19874630b4e02b7b2afecf566b42bbb7e05cba7
Daniel Vidonyi <daniel.vidonyi@googlemail.com>
2019-06-23 18:31:44 +0800

    Updated the hash again to match the updated code

--- dave.rb
+++ dave.rb
@@ -2,7 +2,7 @@ class Dave < Formula
   desc "Simple & intuitive command line utility to store cheat sheet snippets"
   homepage "https://github.com/crocodile/smart-snippets"
   url "https://github.com/crocodile/smart-snippets/raw/master/archive/smart-snippets.tar.gz"
-  sha256 "15e14981c05c8498f7ccc7d445fc754117386545cf382c347039a183b9d85928"
+  sha256 "14a06b15209c89012c17a9866e6f9e2725133f402e0387c08e231f4bfe74a078"
   version "1.0.0"
 
   depends_on "curl"
